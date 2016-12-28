bf7bf910a4906982c395f334bcf8ccd574f8497d
praveendareddy21 <praveendareddy@gmail.com>
2016-12-27 17:43:25 -0800

    cloned abstract ann class files from  other project

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+main driver function
+ */
+
+
+#include <bitset>
+#include<iostream>
+#include<exception>
+#include<vector>
+#include<sstream>
+#include<map>
+#include<math.h>
+
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include"ann.h"
+#include"sigmoid_ann.h"
+#include"relu_ann.h"
+#include"inv_square.h"
+
+using namespace std;
+
+
+
+
+ int main(int argc, char* argv[]){
+
+
+	 Relu_Ann a;
+
+
+	 cout<<endl<<"out in main at the end"<<endl;
+
+	 return 0;
+}
+
