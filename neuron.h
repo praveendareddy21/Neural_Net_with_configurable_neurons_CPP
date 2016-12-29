08dc0963a8d7ff248ef7570de7271ea88146b49b
praveendareddy21 <praveendareddy@gmail.com>
2016-12-29 15:11:20 -0800

    updated neuron class to have referenced attribute
    ( for cleaner code

--- neuron.h
+++ neuron.h
@@ -13,13 +13,19 @@
 #include<iostream>
 #include<math.h>
 #include<iomanip>
+
+using namespace std;
 class Neuron{
 public:
-	Neuron();
-	void doForwardPass(std::vector<std::vector< int> > node, std::vector< std::vector<float_data_type> > *weightTable,
-			std::vector<std::vector<float_data_type> > *output_, int layer, int nodeNum);
-	void doBackwardPass(std::vector<std::vector< int> > node, std::vector< std::vector<float_data_type> > *weightTable,
-			std::vector<std::vector<float_data_type> > *error_ ,int layer, int nodeNum);
+	vector<vector< float_data_type> > &weightTable;
+	vector<vector< float_data_type> > &error;
+	vector<vector< float_data_type> > &output;
+
+	Neuron(vector<vector< float_data_type> > &w , vector<vector< float_data_type> > &e,
+	 vector<vector< float_data_type> > &o );
+	
+	void doForwardPass(int nodeNum);
+	void doBackwardPass(int nodeNum, boolean isOuputLayer, float_data_type OutputValue);
 };
 
 #endif /* NEURON_H_ */
